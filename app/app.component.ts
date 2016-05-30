78e7f2294995bd311c82046a5b278655afda914e
abdul.khoir <abdul.khoir@1rstwap.com>
2016-05-30 19:18:04 +0700

    initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,20 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
+})
+export class AppComponent { 
+	title = 'Tour of Heroes';
+	hero = 'Windstorm';
+}
+
+export class Hero {
+	id: number;
+	name: string;
+}
+
+hero: Hero = {
+	id: 1,
+	name: 'Windstorm'
+}
