4e413229cc4e71d100bb772d40679419bf5043d0
abdul.khoir <abdul.khoir@1rstwap.com>
2016-05-30 20:30:02 +0700

    3. Master/Detail

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,20 +1,44 @@
 import {Component} from '@angular/core';
 
+var HEROES: Hero[] = [
+	{ "id": 11, "name": "Mr. Nice" },
+	{ "id": 12, "name": "Narco" },
+	{ "id": 13, "name": "Bombasto" },
+	{ "id": 14, "name": "Celeritas" },
+	{ "id": 15, "name": "Magneta" },
+	{ "id": 16, "name": "RubberMan" },
+	{ "id": 17, "name": "Dynama" },
+	{ "id": 18, "name": "Dr IQ" },
+	{ "id": 19, "name": "Magma" },
+	{ "id": 20, "name": "Tornado" }
+];
+
 @Component({
     selector: 'my-app',
-    template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
+    template: `
+    <h1>{{title}}</h1>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+    	<li  *ngFor="let hero of heroes">
+    	</li>
+    </ul>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+    	<label>name: </label>
+    	<input [(ngModel)]="hero.name" placeholder="name">
+    </div>`
 })
-export class AppComponent { 
+export class AppComponent {
+	public heroes = HEROES;
 	title = 'Tour of Heroes';
-	hero = 'Windstorm';
+	hero: Hero = {
+		id: 1,
+		name: "Windstrom"
+	}
 }
 
 export class Hero {
 	id: number;
 	name: string;
-}
-
-hero: Hero = {
-	id: 1,
-	name: 'Windstorm'
 }
