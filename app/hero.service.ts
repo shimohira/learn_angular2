d6a4edd9b42fb1f1f352517583b6fa3582c1efd8
abdul.khoir <abdul.khoir@1rstwap.com>
2016-06-01 21:14:54 +0700

    6.Routing

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -3,7 +3,13 @@ import {HEROES} from './mock-heroes';
 
 @Injectable()
 export class HeroService {
-  getHeroes() {
-	  return HEROES;
-  }
+  	getHeroes() {
+	  return Promise.resolve(HEROES);
+	}
+	// See the "Take it slow" appendix
+	/*getHeroesSlowly() {
+	  return new Promise<Hero[]>(resolve =>
+	    setTimeout(()=>resolve(HEROES), 2000) // 2 seconds
+	  );
+	}*/
 }
